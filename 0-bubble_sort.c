5c6f760ffddd5319f5d95a56c5712dad90afdcc8
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-26 02:10:49 +0300

    corret the print out and work with betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,15 +6,15 @@
  *
  * @array: any unsorted array
  * @size: len of an array def sozeof(array)/sizeof(array[0]
- * */
+ **/
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int tmp, swapped = 1;
-	
+
 	if (array == NULL || size < 2)
 		return;
-	
+
 	for (i = 0; i < size - 1 && swapped; i++)
 	{
 		swapped = 0;
@@ -26,10 +26,8 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				swapped = 1;
-					/* print_array(array, size);*/
+				print_array(array, size);
 			}
 		}
-		/*print_array(array, size);*/
 	}
-	print_array(array, size);
 }
