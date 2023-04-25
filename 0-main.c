5313ff1516bae81564a6dd486127801b9cda8ba6
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-26 02:05:34 +0300

    header file for the sorting project

--- 0-main.c
+++ 0-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "sort.h"
 
 /**
@@ -9,13 +10,19 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {1, 8, 10, 11, 12, 17, 19, 21, 22, 24, 25, 28, 31, 32, 34, 35, 36, 37, 38, 40, 41, 46, 47, 48, 50, 51, 52, 53, 56, 57, 59, 60, 61, 65, 66, 68, 69, 71, 72, 73, 74, 76, 81, 83, 89, 92, 93, 95, 96, 98};
+	size_t n = sizeof(array) / sizeof(array[0]);
+	time_t start = time(NULL), end = time(NULL);
+	double total_time = difftime(end, start);
 
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	printf("%s\n",ctime(&start));
+	print_array(array, n);
+	printf("\n");
+	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+	printf("%s\n",ctime(&end));
+	printf("\n%fn", total_time);
+
+	return (0);
 }
