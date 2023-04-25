b5a1ac149875dc670ae890b7145c8b9492118bca
gebretewodros73 <gebretewodros73@gmail.com>
2023-04-26 02:51:55 +0300

    check for betty

--- 0-main.c
+++ 0-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include "sort.h"
 
 /**
@@ -10,15 +9,13 @@
  */
 int main(void)
 {
-	int array[] = {1, 8, 10, 11, 12, 17, 19, 21, 22, 24, 25};
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
 
-	size_t n = sizeof(array) / sizeof(array[0]);
-
-	print_array(array, n);
-	printf("\n");
-	bubble_sort(array, n);
-	printf("\n");
-	print_array(array, n);
-
-	return (0);
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
 }
